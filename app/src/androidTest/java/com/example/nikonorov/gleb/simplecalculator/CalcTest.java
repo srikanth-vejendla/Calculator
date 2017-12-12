eccdf53f5675a3e8d2fd6858dc0ed56678348123
srikanth-vejendla <srikanth.vejendla@outlook.com>
2017-12-12 13:44:30 -0800

    Update CalcTest.java

--- app/src/androidTest/java/com/example/nikonorov/gleb/simplecalculator/CalcTest.java
+++ app/src/androidTest/java/com/example/nikonorov/gleb/simplecalculator/CalcTest.java
@@ -16,67 +16,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-@RunWith(AndroidJUnit4.class)
 public class CalcTest {
-
-    //Launch App
-    @Rule
-    public ActivityTestRule<MainActivity> mainActivityRule = new ActivityTestRule<>(MainActivity.class);
-
-    @Test
-    public void verifyAddTwoNumbers() throws Exception{
-        //2+2=4
-        onView(withId(R.id.twoButton)).perform(click());
-        onView(withId(R.id.adButton)).perform(click());
-        onView(withId(R.id.twoButton)).perform(click());
-        onView(withId(R.id.equalsButton)).perform(click());
-
-        //Result is Four
-        onView(withId(R.id.outputLabel)).check(matches(withText("4")));
-        //Espresso -> work with view
-        //ViewMatchers -> Identify the element
-        //ViewActions -> perform actions on the element
-        //ViewAssertions -> validating
-
-        //100 Lines of source Code
-        //100(Max) Lines of Test Coverage
-        //Sample Change to trigger tests
-    }
-
-    @Test
-    public void mulTwoNumbers(){
-        //4*4 =16
-        onView(withId(R.id.fourButton)).perform(click());
-        onView(withId(R.id.multiplyButton)).perform(click());
-        onView(withId(R.id.fourButton)).perform(click());
-        onView(withId(R.id.equalsButton)).perform(click());
-
-        //Result is 16
-        onView(withId(R.id.outputLabel)).check(matches(withText("16")));
-    }
-
-    @Test
-    public void divTwoNumbers(){
-        //8/4 = 2
-        onView(withId(R.id.eightButton)).perform(click());
-        onView(withId(R.id.divideButton)).perform(click());
-        onView(withId(R.id.fourButton)).perform(click());
-        onView(withId(R.id.equalsButton)).perform(click());
-
-        //Result is 2
-        onView(withId(R.id.outputLabel)).check(matches(withText("2")));
-    }
-
-    @Test
-    public void subTwoNumbers(){
-        //8-4=4
-        onView(withId(R.id.eightButton)).perform(click());
-        onView(withId(R.id.subtractButton)).perform(click());
-        onView(withId(R.id.fourButton)).perform(click());
-        onView(withId(R.id.equalsButton)).perform(click());
-
-        //Result is 4
-        onView(withId(R.id.outputLabel)).check(matches(withText("4")));
-    }
 
 }
